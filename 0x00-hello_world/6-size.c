83a26a32effe7ce05ad9d5dd64ff700ecf61d07e
dan24-T <106978779+dan24-T@users.noreply.github.com>
2022-06-30 23:54:48 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,6 +14,7 @@ char charType;
 printf("Size ofa char: %zu byte\n", sizeof(charType));
 printf("Size of an int %zu bytes\n", sizeof(intType));
 printf("Size of a long int: %zu bytes\n", sizeof(intType));
+printf("Size of a long long int: %zu bytes\n", sizeof(intType)*2);
 printf("Size of a float: %zu bytes\n", sizeof(floatType));
 return (0);
 }
