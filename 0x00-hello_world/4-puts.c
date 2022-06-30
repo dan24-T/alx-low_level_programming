dbfac8f44f17c504142dd77b49f2a59ff220e140
dan24-T <106978779+dan24-T@users.noreply.github.com>
2022-06-30 23:18:59 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,14 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-int main (void)
-
+ * Return: Always 0 (Success)
+ */
 {
-
-	    puts("\"Programming is like building a multilingual puzzle");
-
-	        return 0;
-
+	
+    puts("\"Programming is like building a multilingual puzzle");
+	
+    return (0);
+	
 }
