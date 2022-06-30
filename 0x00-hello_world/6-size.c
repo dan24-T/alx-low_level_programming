f5d683191a3cba457dfe118889eff2662827c3c5
dan24-T <106978779+dan24-T@users.noreply.github.com>
2022-06-30 23:47:40 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
 
 int main(){
-   printf ("Integers: %i %u \n", -3456, 3456);
-   printf ("Characters: %c %c \n", 'z', 80);
-   printf ("Decimals: %d %ld\n", 1997, 32000L);
-   printf ("Some different radices: %d %x %o %#x %#o \n", 100, 100, 100, 100, 100);
-   printf ("floats: %4.2f %+.0e %E \n", 3.14159, 3.14159, 3.14159);
-   printf ("Preceding with empty spaces: %10d \n", 1997);
-   printf ("Preceding with zeros: %010d \n", 1997);
-   printf ("Width: %*d \n", 15, 140);
-   printf ("%s \n", "Educative");
-   return (0);
+int intType;
+float floatType;
+double doubleType;
+char charType;
+printf("Size of int: %zu bytes\n", sizeof(intType));
+printf("Size of float: %zu bytes\n", sizeof(floatType));
+printf("Size of double: %zu bytes\n", sizeof(doubleType));
+printf("Size of char: %zu byte\n", sizeof(charType));
+return (0);
 }
