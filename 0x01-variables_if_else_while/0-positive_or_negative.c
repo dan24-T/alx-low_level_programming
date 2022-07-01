82402170ce078622a911c155af54773370040e2c
dan24-T <106978779+dan24-T@users.noreply.github.com>
2022-07-01 21:51:14 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,15 +5,15 @@ int main(void)
 	int n;
 	if (0<n)
 	{
-		printf("%d: is positive\n");
+		printf("%d",c,": is positive\n");
 	}
 	else if (0=n)
 	{
-		printf("%d: is zero\n");
+		printf("%d",c,": is zero\n");
 	}
 	else
 	{
-		printf("%d: is negative\n");
+		printf("%d",c,": is negative\n");
 	}
 	return (0);
 }
