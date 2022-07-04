8b5bfb1ce5342af98840a37c166ec7967e349634
dan24-T <106978779+dan24-T@users.noreply.github.com>
2022-07-04 15:58:24 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,13 @@
 int main(void)
 {
 int i;
-for (i = 48; i < 58 ; i++)
+int n = 57;
+for (i = 48; i < 57 ; i++)
 {
 putchar(i);
 putchar(',');
 putchar(' ');
 }
-putchar('\n');
+putchar(n);
 return (0);
 }
