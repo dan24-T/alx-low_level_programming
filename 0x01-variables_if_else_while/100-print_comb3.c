2f9d1e9e2440ed69caf7b3c9db508d3b604e9971
dan24-T <danielwaweru9632@gmail.com>
2022-07-04 06:38:07 -0700

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i, j;
+for (i = '0'; i <= '9' ; i++)
+{
+for (j = '0'; j <= '9' ; j++)			        {
+putchar(i);
+putchar(j);
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
