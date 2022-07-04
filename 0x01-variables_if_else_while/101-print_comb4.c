fa912c79cea417319b54513adb9e6b9d8fa29f0a
dan24-T <106978779+dan24-T@users.noreply.github.com>
2022-07-05 02:40:49 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,74 +1,36 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <string.h>
-
 /**
- *
- * * main - Entry point
- *
- * *
- *
- * * Return: Always 0 (Success)
- *
- * */
-
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
-
 {
-
-	int i, j, k;
-
-	for (i = '0'; i <= '9' ; i++)
-
-	{
-
-		for (j = '0'; j <= '9' ; j++)
-
-		{
-
-			for (k = '0'; k <= '9'; k++)
-
-			{
-
-				if (i != j && j != k && i != k)
-
-				{
-
-					if (k > j && j > i)
-
-					{
-
-						putchar(i);
-
-						putchar(j);
-
-						putchar(k);
-
-						if (i != '7')
-
-							putchar(',');
-
-						putchar(' ');
-
-					}
-
-					/****/
-
-				}
-
-				/*****/
-
-			}
-
-		}
-
-	}
-
-	
-
-	return (0);
-
+int i, j, k;
+for (i = '0'; i <= '9' ; i++)
+{
+for (j = '0'; j <= '9' ; j++)
+{
+for (k = '0'; k <= '9'; k++)
+{
+if (i != j && j != k && i != k)
+{
+if (k > j && j > i)
+{
+putchar(i);
+putchar(j);
+putchar(k);
+if (i != '7')
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
 
