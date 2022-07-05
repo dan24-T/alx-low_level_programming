bff11bed5746f474461cd71462e007aca2076f09
dan24-T <106978779+dan24-T@users.noreply.github.com>
2022-07-06 00:51:01 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,3 +1,20 @@
 #include <stdio.h>
+#include "main.h"
 /**
- * */
+* print_alphabet - entry point
+* Description: function print_alphabet(void)
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+int n;
+char i;
+for (n = 0; n <= 10; n++)
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+}
+}
