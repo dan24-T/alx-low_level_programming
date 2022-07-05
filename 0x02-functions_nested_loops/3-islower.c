76baa14245defa1b0f57ed7fb29ca0541d4dca85
dan24-T <106978779+dan24-T@users.noreply.github.com>
2022-07-06 01:28:10 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,3 +1,18 @@
 #include <stdio.h>
+#include "main.h"
 /**
- */
+* print_alphabet - entry point
+* Description: function print_alphabet(void)
+* Return: Always 0 (Success)
+*/
+int _islower(int c)
+{
+if (islower(c))
+{
+putchar('0');
+}
+else
+{
+putchar('1');
+}
+}
