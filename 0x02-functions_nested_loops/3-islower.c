8e7d4d5e8825c6c1a43dbed6e9a6b13c37d5929f
dan24-T <106978779+dan24-T@users.noreply.github.com>
2022-07-06 02:18:55 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,17 +4,17 @@
 /**
 *  _islower - entry point
 * Description: function print_alphabet(void)
-* Return: Always c (Success)
+* Return: Always (c) (Success)
 */
 int _islower(int c)
 {
 if (islower(c))
 {
-c=1;
+c = 1;
 }
 else
 {
-c=0;
+c = 0;
 }
 return (c);
 }
