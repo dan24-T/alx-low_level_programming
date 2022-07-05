90594e12fc7c00cbee18ec8fb1de16de3794bf97
dan24-T <106978779+dan24-T@users.noreply.github.com>
2022-07-06 02:24:39 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+*  _isalpha - entry point
+* Description: function print_alphabet(void)
+* Return: Always (c) (Success)
+*/
+int _isalpha(int c)
+{
+if (isalpha(c))
+{
+c = 1;
+}
+else
+{
+c = 0;
+}
+return (c);
+}
