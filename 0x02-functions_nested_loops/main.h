425b748c096402c18cd3f187ccba46cd7a87422b
dan24-T <106978779+dan24-T@users.noreply.github.com>
2022-07-07 00:51:01 +0300

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,11 +4,13 @@
 int _abs(int);
 int _isalpha(int c);
 int _islower(int c);
+int add(int, int);
 int _putchar(char c);
 int print_sign(int n);
 int print_last_digit(int);
 void print_alphabet();
 void jack_bauer(void);
+void print_to_98(int n);
 void print_alphabet_x10();
 void times_table(void);
 
