32b14cadfad16127e3b2589fea4fa8cc3c683c14
dan24-T <106978779+dan24-T@users.noreply.github.com>
2022-07-06 22:54:44 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*print_last_digit - entry point
+*@i: The character to print
+*Return: Always (x) (Success)
+*/
+int print_last_digit(int i)
+{
+int last_digit, x;
+last_digit = i % 10;
+x = abs(last_digit);
+printf("%d",x);
+return x;
+}
