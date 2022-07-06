b61d2176debf6ebaba20369ce3f81437924d9582
dan24-T <106978779+dan24-T@users.noreply.github.com>
2022-07-07 00:31:48 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,18 +5,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-printf("+");
-n=1;
+_putchar('+');
+return (1);
 }
 else if (n == 0)
 {
-putchar('0');
-n=0;
+_putchar('0');
+return (0);
 }
 else
 {
-putchar('-');
-n=-1;
+_putchar('-');
+return (-1);
 }
-return (n);
 }
