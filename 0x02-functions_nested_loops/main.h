7ffe0d9c57f5126f5825af9b2cc53997bc4bc85f
dan24-T <106978779+dan24-T@users.noreply.github.com>
2022-07-06 22:38:44 +0300

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,6 +6,7 @@ int _isalpha(int c);
 int _islower(int c);
 int _putchar(char c);
 int print_sign(int n);
+int print_last_digit(int);
 void print_alphabet();
 void print_alphabet_x10();
 void times_table(void);
