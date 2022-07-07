2de787e7010eed1599c97e8147b2537162ac9342
dan24-T <106978779+dan24-T@users.noreply.github.com>
2022-07-07 23:36:38 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,15 @@
 #include "main"
+/**
+ * print_numbers - A function that checks for uppercase character.
+ * @i: An input character
+ * Return: 1 if i is uppercase or 0 otherwise
+ */
+void print_numbers(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+_putchar(i);
+}
+return (0);
+}
